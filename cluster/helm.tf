5d3351847dbcf21d2d3d195958a53c62faa9bf25
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-02-12 15:31:52 +0530

    helm install

--- cluster/helm.tf
+++ cluster/helm.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.17.0"
-    }
-  }
-}
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
