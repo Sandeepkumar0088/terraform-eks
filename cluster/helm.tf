561d2609c9d2f04b1378f28d1c2aaa2cfd4786a4
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-02-23 15:36:01 +0530

    terraform eks

--- cluster/helm.tf
+++ cluster/helm.tf
@@ -5,16 +5,34 @@ provider "helm" {
 }
 
 # resource "helm_release" "argo-cd" {
-#   depends_on = [ null_resource.kubeconfig ]
+#   depends_on = [ null_resource.kubeconfig, helm_release.nginx-ingress ]
 #
 #   name  = "argocd"
 #   repository = "https://argoproj.github.io/argo-helm"
 #   chart = "argo-cd"
-#
+
+# set = [
+#   {
+#     name  = "server.ingress.enabled"
+#     value = "true"
+#   },
+#   {
+#     name  = "server.ingress.ingressClassName"
+#     value = "nginx"
+#   },
+#   {
+#     name  = "global.domain"
+#     value = "argocd-${var.env}.sandeepkumar0088.online"
+#   },
+#   {
+#     name  = "configs.params.server\\.insecure"
+#     value = "true"
+#   }
+# ]
 # }
 
 resource "helm_release" "prometheus-stack" {
-  depends_on  = [ null_resource.kubeconfig ]
+  depends_on  = [ null_resource.kubeconfig, helm_release.nginx-ingress ]
 
   name        = "promstack"
   repository  = "https://prometheus-community.github.io/helm-charts"
@@ -23,14 +41,25 @@ resource "helm_release" "prometheus-stack" {
 
   set = [
     {
-      name  = "prometheus.service.type"
-      value = "LoadBalancer"
-    },
-    {
       name  = "grafana.enabled"
       value = "false"
+    },
+    {
+      name  = "prometheus.ingress.enabled"
+      value = "true"
+    },
+    {
+      name  = "prometheus.ingress.ingressClassName"
+      value = "nginx"
     }
   ]
+  set_list = [
+    {
+      name  = "prometheus.ingress.hosts"
+      value = ["prometheus-${var.env}.sandeepkumar.online"]
+    }
+  ]
+
 }
 
 resource "helm_release" "nginx-ingress" {
@@ -39,14 +68,27 @@ resource "helm_release" "nginx-ingress" {
   name  = "ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
-
+  set = [
+    {
+      name  = "controller.metrics.enabled"
+      value = "true"
+    },
+    {
+      name  = "controller.podAnnotations.prometheus\\.io/port"
+      value = "10254"
+    },
+    {
+      name  = "controller.podAnnotations.prometheus\\.io/scrape"
+      value = "true"
+    }
+  ]
 }
 
-# resource "helm_release" "external-dns" {
-#   depends_on = [ null_resource.kubeconfig ]
-#
-#   name  = "external-dns"
-#   repository = "https://kubernetes-sigs.github.io/external-dns"
-#   chart = "external-dns"
-#
-# }
+ # resource "helm_release" "external-dns" {
+ #   depends_on = [ null_resource.kubeconfig ]
+ #
+ #   name  = "external-dns"
+ #   repository = "https://kubernetes-sigs.github.io/external-dns"
+ #   chart = "external-dns"
+ #
+ # }
