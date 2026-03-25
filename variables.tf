2d5cec3018eed5c3e8cea94ee520e3e5b451ae4f
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-03-25 21:00:34 +0530

    terraform eks

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "zone_id" {
 variable "components" {
   default = {
     mongodb   = "t3.small"
-    mysql     = "t3.large"
+    mysql     = "t3.small"
     rabbitmq  = "t3.small"
     redis     = "t3.small"
   }
