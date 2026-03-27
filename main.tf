ecb750a292ba747f5169900a305187743ffd19e4
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-03-27 16:16:38 +0530

    terraform eks

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.cluster.arn
   version  = "1.34"
   vpc_config {
-    subnet_ids = ["subnet-0b41036a19ffb50d9", "subnet-03cd379790a054a51"]
+    subnet_ids      = ["subnet-0f9ae3137a5e6eed2", "subnet-0ddd2890caaca5847"]
   }
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
@@ -22,7 +22,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "main"
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = ["subnet-0b41036a19ffb50d9", "subnet-03cd379790a054a51"]
+  subnet_ids      = ["subnet-0f9ae3137a5e6eed2", "subnet-0ddd2890caaca5847"]
   # instance_types  = ["t3.small"]
   instance_types  = ["c7i-flex.large"]
   capacity_type   = "SPOT"
