591096450ffc9e2c08ac50238675918af956f7fe
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-03-27 16:49:23 +0530

    terraform eks

--- main.tf
+++ main.tf
@@ -68,12 +68,15 @@ resource "null_resource" "kubeconfig" {
 
   provisioner "local-exec" {
     command = <<EOT
-    rm -rf ~/.kube
+
     aws eks update-kubeconfig --name dev
     kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
   EOT
   }
 }
+
+# rm -rf ~/.kube
+# aws eks update-kubeconfig --name dev
 # kubectl get nodes
 # kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
